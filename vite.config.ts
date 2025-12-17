46e6fff903c4ef7c0415e7896c62d431cc1ff9ee
jimmygu <jimmygujh@gmail.com>
2025-12-17 14:52:43 +0800

    first commit

---
+++ vite.config.ts
@@ -0,0 +1,40 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
+import { nodePolyfills } from "vite-plugin-node-polyfills";
+
+export default defineConfig({
+  plugins: [
+    react(),
+    nodePolyfills({
+      include: ["buffer", "process", "stream", "util"],
+      globals: {
+        Buffer: true,
+        global: true,
+        process: true
+      }
+    })
+  ],
+  server: {
+    port: 3001,
+    open: true,
+  },
+  build: {
+    outDir: 'dist',
+    sourcemap: true,
+  },
+  resolve: {
+    alias: {
+      buffer: 'buffer',
+      '@': resolve(__dirname, './src'),
+    },
+  },
+  define: {
+    'process.env': {},
+    global: 'globalThis',
+  },
+  optimizeDeps: {
+    include: ["buffer", "process", "stream", "util", "near-api-js"],
+  },
+});
+
